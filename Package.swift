65bc0da3a3993fbd2880a7076d2f9f456c01224c
K Ram Mohanraj <ram@fuzed.io>
2018-05-22 00:47:28 +0100

    Package update

--- Package.swift
+++ Package.swift
@@ -4,16 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "msa-fragments-service",
+    name: "fragments-service",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // Package with core swift extensions
+        .package(url: "git@github.com:fuzed-innovations/swifty-extensions.git", from: "1.0.0"),
+        // Package with core swift microservice code
+        .package(url: "git@github.com:fuzed-innovations/swifty-microservice.git", from: "2.0.0-rc.2"),
+        // Package for Async handling
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "4.0.0"),
+        // Package for networking
+        .package(url: "git@github.com:fuzed-innovations/networking-queue.git", .upToNextMinor(from: "3.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "msa-fragments-service",
-            dependencies: []),
+            name: "fragments-service",
+            dependencies: ["SwiftyMicroservice",
+                           "PromiseKit",
+                           "SwiftyExtensionsFoundation",
+                           "SwiftyExtensionsCoreLocation",
+                           "SwiftyExtensionsJSON",
+                           "SwiftyExtensionsPromiseKit",
+                           "SwiftyExtensionsPromiseKitJSON",
+                           "NetworkingQueue"]),
     ]
 )
